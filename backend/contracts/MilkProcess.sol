40f5636624c30967d6c4c476c8ebf77c71e0daae
Leonardo Vincenzi <leovincenzi98@gmail.com>
2024-07-19 16:34:48 +0200

    Add comments to contracts

--- backend/contracts/MilkProcess.sol
+++ backend/contracts/MilkProcess.sol
@@ -3,51 +3,61 @@ pragma solidity ^0.8.0;
 
 import "./MilkProcessFactory.sol";
 
+// The MilkProcess contract tracks and manages the steps in the milk processing workflow.
 contract MilkProcess {
 
+    // Step struct represents a single step in the milk processing workflow.
     struct Step {
-        string name;
-        address supervisor;
-        bool completed;
-        uint startTime;
-        uint endTime;
-        string location;
-        uint lotNumber;
-        bool failed;
+        string name;           // Name of the step
+        address supervisor;    // Address of the supervisor responsible for this step
+        bool completed;        // Indicates if the step is completed
+        uint startTime;        // Timestamp when the step started
+        uint endTime;          // Timestamp when the step ended
+        string location;       // Location where the step took place
+        uint lotNumber;        // Lot number associated with this step
+        bool failed;           // Indicates if the step has failed
     }
 
+    // Array to store all steps in the process.
     Step[] public steps;
+    // Index of the current step being executed.
     uint public currentStepIndex;
+    // Lot number for the current process.
     uint public lotNumber;
+    // Address of the factory contract.
     address public factory;
+    // Indicates if the process has failed.
     bool public isFailed;
+    // Indicates if the process is for intero (whole) milk.
     bool public isIntero;
 
+    // Constructor initializes the contract with the lot number, factory address, and milk type.
     constructor(uint _lotNumber, address _factory, bool _isIntero) {
         lotNumber = _lotNumber;
         factory = _factory;
         isFailed = false;
         isIntero = _isIntero;
-        initializeSteps();
-        steps[0].startTime = block.timestamp;
+        initializeSteps();                // Initialize the steps in the process
+        steps[0].startTime = block.timestamp;  // Set the start time of the first step
     }
 
+    // Internal function to initialize the steps based on the milk type.
     function initializeSteps() internal {
         steps.push(Step("Raccolta", address(0), false, 0, 0, "", lotNumber, false));
         steps.push(Step("Trasporto", address(0), false, 0, 0, "", lotNumber, false)); // Supervisore automatico
         steps.push(Step("Pulitura", address(0), false, 0, 0, "", lotNumber, false)); 
         steps.push(Step("Standardizzazione", address(0), false, 0, 0, "", lotNumber, false));
         steps.push(Step("Omogeneizzazione", address(0), false, 0, 0, "", lotNumber, false));
-        if(isIntero){
+        if (isIntero) {
             steps.push(Step("Pastorizzazione", address(0), false, 0, 0, "", lotNumber, false)); // Supervisore automatico
-        }else{
+        } else {
             steps.push(Step("Sterilizzazione", address(0), false, 0, 0, "", lotNumber, false)); // Supervisore automatico
         }
         steps.push(Step("Confezionamento", address(0), false, 0, 0, "", lotNumber, false));
-        if(isIntero){
+        if (isIntero) {
             steps.push(Step("Stoccaggio refrigerato", address(0), false, 0, 0, "", lotNumber, false)); // Supervisore automatico
             steps.push(Step("Distribuzione refrigerata", address(0), false, 0, 0, "", lotNumber, false));
-        }else{
+        } else {
             steps.push(Step("Stoccaggio", address(0), false, 0, 0, "", lotNumber, false)); // Supervisore automatico
             steps.push(Step("Distribuzione", address(0), false, 0, 0, "", lotNumber, false));
         }
@@ -55,6 +65,7 @@ contract MilkProcess {
         currentStepIndex = 0;
     }
 
+    // Assigns supervisors to specific steps in the process.
     function assignSupervisors(address[] memory supervisors) external onlyAdmin {
         require(steps.length > 0, "Steps array cannot be empty");
         uint j = 0;
@@ -66,6 +77,7 @@ contract MilkProcess {
         }
     }
 
+    // Completes the current step and records the location where it was completed.
     function completeStep(string memory _location) external onlySupervisor {
         require(!isFailed, "Process has already failed");
         require(currentStepIndex < steps.length, "All steps are already completed");
@@ -82,6 +94,7 @@ contract MilkProcess {
         }
     }
 
+    // Marks the current step as failed.
     function failStep() external onlySupervisor {
         require(!isFailed, "Process has already failed");
         require(currentStepIndex < steps.length, "All steps are already completed");
@@ -92,12 +105,13 @@ contract MilkProcess {
         currentStepIndex++;
     }
 
+    // Validates the temperature and completes or fails the step accordingly.
     function isTemperatureOK(bool valid) external {
         require(!isFailed, "Process has already failed");
 
         Step storage step = steps[currentStepIndex];
 
-        if (valid){
+        if (valid) {
             step.completed = true;
             step.endTime = block.timestamp;
             step.location = "Procedure up to standards";
@@ -105,33 +119,34 @@ contract MilkProcess {
             if (currentStepIndex < steps.length) {
                 steps[currentStepIndex].startTime = block.timestamp;
             }
-        }else{
+        } else {
             step.failed = true;
             isFailed = true;
             step.endTime = block.timestamp;
-            if (currentStepIndex == 1){
-              step.location = "Road transport failed";
-            } else if ((currentStepIndex == 7 || currentStepIndex == 8) && isIntero){
+            if (currentStepIndex == 1) {
+                step.location = "Road transport failed";
+            } else if ((currentStepIndex == 7 || currentStepIndex == 8) && isIntero) {
                 step.location = "Storage failed";
             }
         }
     }
 
+    // Validates the location and completes or fails the step accordingly.
     function isLocationReasonable(bool valid, string memory _location) external {
         Step storage step = steps[currentStepIndex];
-        if (valid){
+        if (valid) {
             step.completed = true;
             step.endTime = block.timestamp;
             if (currentStepIndex == 9) {
                 step.location = _location;
-            }else{
+            } else {
                 step.location = "Procedure up to standards";
             }
             currentStepIndex++;
             if (currentStepIndex < steps.length) {
                 steps[currentStepIndex].startTime = block.timestamp;
             }
-        }else{
+        } else {
             step.failed = true;
             isFailed = true;
             step.endTime = block.timestamp;
@@ -139,14 +154,17 @@ contract MilkProcess {
         }
     }
 
+    // Checks if the entire process is completed.
     function isProcessCompleted() external view returns (bool) {
         return currentStepIndex >= steps.length;
     }
 
+    // Returns all the steps in the process.
     function getSteps() external view returns (Step[] memory) {
         return steps;
     }
 
+    // Modifier to restrict access to only admins.
     modifier onlyAdmin() {
         MilkProcessFactory factoryContract = MilkProcessFactory(factory);
         MilkProcessFactory.Role role = factoryContract.getRole(msg.sender);
@@ -154,6 +172,7 @@ contract MilkProcess {
         _;
     }
 
+    // Modifier to restrict access to only supervisors.
     modifier onlySupervisor() {
         MilkProcessFactory factoryContract = MilkProcessFactory(factory);
         MilkProcessFactory.Role role = factoryContract.getRole(msg.sender);
--- backend/contracts/MilkProcessFactory.sol
+++ backend/contracts/MilkProcessFactory.sol
@@ -3,67 +3,85 @@ pragma solidity ^0.8.0;
 
 import "./MilkProcess.sol";
 
+// The MilkProcessFactory contract is responsible for creating and managing MilkProcess instances
+// and handling user roles and permissions.
 contract MilkProcessFactory {
+    // Enum to define different user roles
     enum Role { None, Admin, Supervisor, Operator }
 
+    // Array to store addresses of all MilkProcess contracts created by this factory
     address[] public processes;
+    
+    // Mapping from user addresses to their roles
     mapping(address => Role) public roles;
+    
+    // Array to store all user addresses
     address[] public users;
+    
+    // Counter to generate unique lot numbers for new MilkProcess contracts
     uint lotNumber = 0;
 
+    // Constructor initializes the contract, sets the deployer as Admin, and adds them to the users list
     constructor() {
         roles[msg.sender] = Role.Admin;
         users.push(msg.sender);
     }
 
+    // Creates new MilkProcess instances and returns their addresses
     function createNewProcess(uint quantity, bool isIntero) external onlyAdmin returns (address[] memory) {
         address[] memory newProcesses = new address[](quantity);
         for (uint i = 0; i < quantity; i++) {
-            lotNumber = lotNumber + 1;
+            lotNumber = lotNumber + 1;  // Increment lot number for each new process
             MilkProcess newProcess = new MilkProcess(lotNumber, address(this), isIntero);
-            processes.push(address(newProcess));
-            newProcesses[i] = address(newProcess);
+            processes.push(address(newProcess));  // Store the address of the new MilkProcess
+            newProcesses[i] = address(newProcess);  // Add address to the return array
         }
         return newProcesses;
     }
 
+    // Returns the addresses of all MilkProcess contracts created by this factory
     function getAllProcesses() external view returns (address[] memory) {
         return processes;
     }
 
+    // Returns the roles and addresses of all users
     function getAllRoles() external view returns (string[] memory, address[] memory) {
         string[] memory userRoles = new string[](users.length);
         for (uint i = 0; i < users.length; i++) {
             if (roles[users[i]] == Role.Admin) {
-                userRoles[i] = "1";
+                userRoles[i] = "1";  // Admin role
             } else if (roles[users[i]] == Role.Supervisor) {
-                userRoles[i] = "2";
+                userRoles[i] = "2";  // Supervisor role
             } else if(roles[users[i]] == Role.Operator) {
-                userRoles[i] = "3";
+                userRoles[i] = "3";  // Operator role
             } else {
-                userRoles[i] = "0";
+                userRoles[i] = "0";  // No role assigned
             }
         }
         return (userRoles, users);
     }
 
+    // Returns the role of a specific user
     function getRole(address account) external view returns (Role) {
         return roles[account];
     }
 
+    // Assigns a role to a user and adds them to the users list if not already present
     function assignRole(address account, Role role) external onlyAdmin {
         roles[account] = role;
         if (!isAccountInList(account)) {
-            users.push(account);
+            users.push(account);  // Add account to users list if not already present
         }
     }
 
+    // Removes a role from a user and removes them from the users list
     function removeRole(address account) external onlyAdmin {
-        require(roles[account] != Role.Admin || countAdmins() > 1, "Non puoi rimuovere l'ultimo admin");
+        require(roles[account] != Role.Admin || countAdmins() > 1, "Cannot remove the last admin");
         roles[account] = Role.None;
         removeAccountFromList(account);
     }
 
+    // Counts the number of admins in the users list
     function countAdmins() internal view returns (uint) {
         uint count = 0;
         for (uint i = 0; i < users.length; i++) {
@@ -74,6 +92,7 @@ contract MilkProcessFactory {
         return count;
     }
 
+    // Checks if a specific account is in the users list
     function isAccountInList(address account) internal view returns (bool) {
         for (uint i = 0; i < users.length; i++) {
             if (users[i] == account) {
@@ -83,16 +102,18 @@ contract MilkProcessFactory {
         return false;
     }
 
+    // Removes a specific account from the users list
     function removeAccountFromList(address account) internal {
         for (uint i = 0; i < users.length; i++) {
             if (users[i] == account) {
-                users[i] = users[users.length - 1];
-                users.pop();
+                users[i] = users[users.length - 1];  // Replace with the last element
+                users.pop();  // Remove the last element
                 break;
             }
         }
     }
 
+    // Modifier to restrict access to functions to only admins
     modifier onlyAdmin() {
         require(roles[msg.sender] == Role.Admin, "Only admin can perform this action");
         _;
